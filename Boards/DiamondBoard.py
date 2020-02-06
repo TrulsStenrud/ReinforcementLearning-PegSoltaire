f080ef0bb1a61ba0396853f0119adf360eb41c9b
Stenrud <truls.stenrud@gmail.com>
2020-02-06 09:53:18 +0100

    Create a diamondboard that works as 5x5

--- Boards/DiamondBoard.py
+++ Boards/DiamondBoard.py
@@ -19,3 +19,22 @@ class DiamondBoard(HexBoard):
 
         return board
 
+
+class SortOfDiamondBoard(HexBoard):
+    def __init__(self):
+        board = self.makeBoard(5)
+
+        extra = list()
+        extra.append(0)
+        board.append(extra)
+        super().__init__(tuple(map(lambda x: tuple(x), board)))
+
+    def makeBoard(self, size):
+        board = []
+        for i in range(0, size):
+            board.append([])
+            for j in range(0, size):
+                board[i].append(1)
+
+        return board
+
