5bbd721335293d267a8d4f553581ecd18fe90d62
Stenrud <truls.stenrud@gmail.com>
2020-02-19 23:31:19 +0100

    Truly final

--- Main.py
+++ Main.py
@@ -3,34 +3,9 @@ import matplotlib.pyplot as plt
 from GraphicalUserInterface import GraphicalUserInterface
 from RL import RL
 
-
-def draw_state(state):
-    G = nx.Graph()
-
-    edges = state.get_edges()
-    for edge in edges:
-        G.add_edge(edge[0], edge[1])
-    colors = list()
-    board = state.get_board()
-    for node in G.nodes:
-        if board[node[0]][node[1]] == 0:
-            colors.append('red')
-        else:
-            colors.append('black')
-    my_pos = nx.spring_layout(G, iterations=50)
-
-    nx.draw(G, pos=my_pos, node_color=colors)
-    plt.show()
-
-
 def main():
-    states = RL.reinforcement_learning()
-    draw_state(states[0][0])
-
-
-def testing():
-    a = GraphicalUserInterface()
+    _ = GraphicalUserInterface()
 
 
 if __name__ == "__main__":
-    testing()
+    main()
